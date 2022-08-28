f1aee3e1f3ca1fbc0a15c70b1e7d4bfd5b2123fe
VBychenkova <timka_@bk.ru>
2022-08-28 15:22:19 +0300

    add develop

---
+++ src/main/java/ru/ibs/cources/gitsimpleproject3/Gitsimpleproject3Application.java
@@ -0,0 +1,13 @@
+package ru.ibs.cources.gitsimpleproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitsimpleproject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitsimpleproject3Application.class, args);
+    }
+
+}
