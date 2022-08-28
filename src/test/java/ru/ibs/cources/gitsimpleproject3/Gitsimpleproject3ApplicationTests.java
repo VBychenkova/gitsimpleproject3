f2fa561f8fac0dce4f968dd72920c9be65bdff6d
VBychenkova <timka_@bk.ru>
2022-08-28 15:22:19 +0300

    init simple project

---
+++ src/test/java/ru/ibs/cources/gitsimpleproject3/Gitsimpleproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ibs.cources.gitsimpleproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitsimpleproject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
